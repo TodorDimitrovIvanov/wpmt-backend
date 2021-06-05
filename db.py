f6fa927a7879960123847220afa65619730734bf
todor <todor.ivanov.work@gmail.com>
2021-06-06 02:45:08 +0300

    Added functionality for adding and listing sites. Also added comments for separating the sections of the API

--- db.py
+++ db.py
@@ -1,4 +1,5 @@
 import sqlite3
+import json
 from sqlite3 import Error
 # Source: https://www.sqlitetutorial.net/sqlite-python/
 
@@ -20,6 +21,7 @@ class DB:
         sql_command = None
         with open(db_source, 'r') as sql_file:
             sql_command = sql_file.read()
+        # TODO: Add error handling and reporting
         db_conn = DB.db_conn_start(db_file)
         cursor = db_conn.cursor()
         # Source: https://stackoverflow.com/questions/32372353/how-to-create-a-db-file-in-sqlite3-using-a-schema-file-from-within-python
@@ -31,12 +33,109 @@ class DB:
     def db_struct_list(db_file):
         # Source: https://stackoverflow.com/questions/305378/list-of-tables-db-schema-dump-etc-using-the-python-sqlite3-api
         sql_command = "SELECT name FROM sqlite_master WHERE type ='table' AND name NOT LIKE 'sqlite_%';"
+        # TODO: Add error handling and reporting
         db_conn = DB.db_conn_start(db_file)
         db_cursor = db_conn.cursor()
         db_cursor.execute(sql_command)
-        print(db_cursor.fetchall())
+        temp = db_cursor.fetchall()
+        db_conn.close()
+        return temp
+
+    ###################
+    ###### USERS ######
+    @staticmethod
+    def db_user_add(db_file, client_id, client_key, email, name, service, notifications, icon):
+        sql_prep = "INSERT INTO users (client_id, client_key, email, name, service, notifications, icon) VALUES (?,?,?,?,?,?,?);"
+        sql_data = [client_id, client_key, email, name, service, notifications, icon]
+        # TODO: Add error handling and reporting
+        db_conn = DB.db_conn_start(db_file)
+        db_cur = db_conn.cursor()
+        db_cur.execute(sql_prep, sql_data)
+        db_conn.commit()
         db_conn.close()
 
     @staticmethod
-    def db_add_user(db_file, client_id, client_key, email, name, service, notifications, icon):
-        pass
+    def db_user_get(db_file, client_id):
+        sql_prep = "SELECT * FROM users WHERE client_id=?"
+        sql_data = [client_id]
+        db_conn = DB.db_conn_start(db_file)
+        db_cur = db_conn.cursor()
+        db_cur.execute(sql_prep, sql_data)
+        result = db_cur.fetchall()
+        db_conn.close()
+        return result
+
+    @staticmethod
+    def db_user_all(db_file):
+        # Here prepare the SQL request and gather the data
+        sql_prep = "SELECT * FROM users"
+        db_conn = DB.db_conn_start(db_file)
+        db_cur = db_conn.cursor()
+        db_cur.execute(sql_prep)
+        columns = list(map(lambda x: x[0], db_cur.description))
+        data = db_cur.fetchall()
+
+        # Here we transform the data into JSON format
+        temp_dict = {}
+        result = {}
+        for entry in range(len(data)):
+            for index in range(len(columns)):
+                item = data[entry]
+                key = columns[index]
+                temp_dict[key] = item[index]
+            result[entry] = json.dumps(temp_dict, indent=4, separators=('\n', ','))
+        # The "result" is a dictionary of dictionaries.
+        # Each dictionary contains the keys and values from the "users" table
+        return result
+    ###### USERS ######
+    ###################
+
+
+    #####################
+    ###### WEBSITE ######
+    @staticmethod
+    def db_site_add(db_file, website_id, client_id, domain, dom_exp, certificate, cert_exp):
+        sql_prep = "INSERT INTO website (website_id, client_id, domain, domain_expiry, certificate, certificate_expiry) VALUES (?,?,?,?,?,?);"
+        sql_data = [website_id, client_id, domain, dom_exp, certificate, cert_exp]
+        # TODO: Add error handling and reporting
+        db_conn = DB.db_conn_start(db_file)
+        db_cur = db_conn.cursor()
+        db_cur.execute(sql_prep, sql_data)
+        db_conn.commit()
+        db_conn.close()
+
+    @staticmethod
+    def db_site_get(db_file, website_id):
+        sql_prep = "SELECT * FROM website WHERE website_id=?"
+        sql_data = [website_id]
+        # TODO: Add error handling and reporting
+        db_conn = DB.db_conn_start(db_file)
+        db_cur = db_conn.cursor()
+        db_cur.execute(sql_prep, sql_data)
+        result = db_cur.fetchall()
+        db_conn.close()
+        return result
+
+    @staticmethod
+    def db_site_all(db_file, client_id):
+        # Here prepare the SQL request and gather the data
+        sql_prep = "SELECT * FROM website WHERE client_id=?"
+        sql_data = [client_id]
+        db_conn = DB.db_conn_start(db_file)
+        db_cur = db_conn.cursor()
+        db_cur.execute(sql_prep, sql_data)
+        columns = list(map(lambda x: x[0], db_cur.description))
+        data = db_cur.fetchall()
+
+        # Here we transform the data into JSON format
+        temp_dict = {}
+        result = {}
+        for entry in range(len(data)):
+            for index in range(len(columns)):
+                item = data[entry]
+                key = columns[index]
+                temp_dict[key] = item[index]
+            result[entry] = json.dumps(temp_dict, indent=4, separators=('\n', ','))
+        # The "result" is a dictionary of dictionaries.
+        # Each dictionary contains the keys and values from the "website" table
+        return result
--- router.py
+++ router.py
@@ -9,32 +9,81 @@ import uvicorn
 
 app = FastAPI()
 
+# -------------------------
 # Temporary code
 # When live the API should be able to retrieve the DB settings (and others) from the config.json file
 db_source = "/home/todor/DbSchema/Schemas/wpmt-prototype-exported.sql"
 db_file = "/home/todor/WPMT/db/wpmt.db"
+
+####################
+#### DUMMY DATA ####
+# We use these variables for testing the DB calls
+client_id = "UEU00002"
+client_key = "UEU00002-K1"
+email = "test@domain.com"
+name = "Test Dummy"
+service = "Free"
+notifications = "Disabled"
+icon = "None"
+
+website_id = "UEU00002-W1"
+domain = "domain.com"
+domain_exp = "02-Jun-2021"
+certificate = "None"
+cert_exp = "None"
+#### DUMMY DATA ####
+####################
+# -------------------------
+
 
 # Here we define the web-root path
 # It will then route the request to the setup function
 @app.get("/")
 def read_root():
     if File.db_setup() and File.config_setup():
-        #DB.db_init(db_source, db_file)
-        DB.db_struct_list(db_file)
         return {"Init: Success"}
     else:
         return {"Init: Failure"}
 
 
 @app.post("/user/add")
-def user_add():
-    pass
+async def user_add():
+    DB.db_user_add(db_file,client_id,client_key,email,name,service,notifications,icon)
 
 
 @app.get("/user/get", status_code=200)
 def user_get():
-    pass
+    result = DB.db_user_get(db_file, client_id)
+    return result
 
+
+@app.get("/user/all", status_code=200)
+def user_all():
+    result = DB.db_user_all(db_file)
+    return result
+
+
+@app.get("/db/structure", status_code=200)
+def db_struct_get():
+    result = DB.db_struct_list(db_file)
+    return result
+
+
+@app.post("/site/add", status_code=200)
+def db_site_add():
+    DB.db_site_add(db_file, website_id, client_id, domain, domain_exp, certificate, cert_exp)
+
+
+@app.get("/site/get", status_code=200)
+def db_site_get():
+    result = DB.db_site_get(db_file, website_id)
+    return result
+
+
+@app.get("/site/all", status_code=200)
+def db_site_all():
+    result = DB.db_site_all(db_file, client_id)
+    return result
 
 if __name__ == '__main__':
     # TODO: Add error handling for when the default port is not available
